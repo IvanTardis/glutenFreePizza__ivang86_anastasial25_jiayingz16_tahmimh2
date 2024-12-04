fbb413837dca7690d67307ee977fd9f2512fd508
Tahmim Hassan <thassan50@stuy.edu>
2024-12-04 10:40:05 -0500

    added login register and logout

--- app/__init__.py
+++ app/__init__.py
@@ -24,6 +24,61 @@ c = db.cursor()  #facilitate db ops -- you will use cursor to trigger db events
 
 @app.route(("/"), methods=['GET', 'POST'])
 def home():
+    if 'username' in session:
+        return render_template("home.html", user = session['username'])
+    else:
+        return redirect("/login")
+
+
+# USER LOGIN
+@app.route('/login', methods=['GET','POST'])
+def login():
+    return render_template("login.html")
+
+@app.route('/auth_login', methods=["GET", "POST"])
+def auth_login():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        app.secret_key = os.urandom(32)
+        if dbx.verify_user(username, password):
+            session['username'] = username
+            session['name'] = username
+            return redirect('/')
+        else:
+            flash("Incorrect username or password.", 'error')
+            return redirect("/login")
+
+
+# USER REGISTRATIONS
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template("register.html")
+
+@app.route('/auth_reg', methods=["GET", "POST"])
+def auth_reg():
+    if request.method == "POST":
+        new_username = request.form['new_user']
+        new_password = request.form['new_pass']
+        elif new_password != request.form['confirm_pass']:
+            flash("Passwords do not match.", 'error')
+            return render_template("register.html")
+        else:
+            try:
+                dbx.create_user(new_username, new_password)
+                flash("You are now registered! Please log in.", 'success')
+                return render_template("login.html")
+            except sqlite3.IntegrityError:
+                flash("Username already exists.", 'error')
+                return render_template("register.html")
+
+
+# USER LOGOUTS
+@app.route('/logout', methods=["GET", "POST"])
+def logout():
+    session.pop('username', None)
+    session.pop('name', None)
+    return redirect("/")
 
 
 if __name__ == "__main__": #false if this file imported as module
